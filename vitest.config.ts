1995b74a409a512127faa47f73c606f49afb16eb
taskylizard <75871323+taskylizard@users.noreply.github.com>
2023-12-11 04:47:40 +0000

    chore: setup vitest

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import { resolve } from "path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    root: resolve(__dirname),
+    globals: true,
+  },
+});
